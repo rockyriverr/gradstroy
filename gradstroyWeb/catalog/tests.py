3a97d135c554472513db13935fd3ffa7b3320212
rockyriverr <gavrikova.as@phystech.edu>
2020-05-24 23:25:39 +0300

    frog tests

--- gradstroyWeb/catalog/tests.py
+++ gradstroyWeb/catalog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from catalog.models import Product
 # Create your tests here.
+
+
+class ProductTestCase(TestCase):
+    def test_product_save(self):
+        product = Product(name='lyagushka', img='lyagushka.jpg', description='лягушка', price=10000)
+        product.save()
+
+    def test_product_in_database(self):
+        self.test_product_save()
+        frog = Product.objects.get(name='lyagushka')
+        assert frog
